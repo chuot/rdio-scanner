5e6237afba7fdb731a42dc6b1da217dfc616b2fc
Chrystian Huot <chrystian.huot@rhfaction.com>
2019-07-18 08:54:29 -0400

    Add database indexes for searchable fields

--- server/imports/fixtures/calls.ts
+++ server/imports/fixtures/calls.ts
@@ -1,4 +1,6 @@
 import { Calls } from '../../../imports/collections/calls';
 
 Calls.rawCollection().createIndex({ createdAt: 1 }, { name: '_createdAt_' });
-Calls.rawCollection().createIndex({ startTime: 1 }, { name: '_startTime_' });
+Calls.rawCollection().createIndex({ startTime: 1 }, { name: '_startTimeAsc_' });
+Calls.rawCollection().createIndex({ system: 1 }, { name: '_system_' });
+Calls.rawCollection().createIndex({ talkgroup: 1 }, { name: '_talkgroup_' });
