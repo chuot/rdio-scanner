4b23284a83109d9d27fba2e3fd6ff23f14fd9875
Chrystian Huot <chrystian.huot@saubeo.solutions>
2020-05-21 10:28:38 -0400

    Fix dirWatch for trunk-recorder conventional systems

--- server/lib/rdio-scanner/dir-watch.js
+++ server/lib/rdio-scanner/dir-watch.js
@@ -31,153 +31,175 @@ class DirWatch {
         this.watchers = [];
 
         this.app.config.dirWatch.forEach((dirWatch = {}) => {
-            if (typeof dirWatch.directory === 'string' && dirWatch.directory.length) {
-                const dir = path.resolve(__dirname, '../../..', dirWatch.directory);
+            if (typeof dirWatch.directory !== 'string' || !dirWatch.directory.length) {
+                console.error(`Invalid dirWatch.directory: ${dirWatch.directory}`);
 
-                const watcher = chokidar.watch(dir, {
-                    awaitWriteFinish: true,
-                    ignoreInitial: true,
-                    recursive: true,
-                    usePolling: typeof dirWatch.usePolling === 'boolean' ? dirWatch.usePolling : false,
-                });
-
-                watcher.on('add', async (filename) => {
-                    const file = path.parse(filename);
-
-                    if (
-                        (Array.isArray(dirWatch.extension) && dirWatch.extension.some((ext) => file.ext === `.${ext}`)) ||
-                        (file.ext === `.${dirWatch.extension}`)
-                    ) {
-                        const parse = (val) => {
-                            if (typeof val === 'number') {
-                                return val;
-
-                            } else if (typeof val === 'string') {
-                                try {
-                                    const regExp = new RegExp(val);
-
-                                    return parseInt(filename.replace(regExp, '$1'), 10);
-
-                                } catch (error) {
-                                    console.log(`Invalid RegExp: ${val}`);
-
-                                    return NaN;
-                                }
-
-                            } else {
-                                return NaN;
-                            }
-                        }
-
-                        const audio = fs.readFileSync(filename);
-
-                        const audioName = file.base;
-
-                        const audioType = mime.lookup(file.base);
-
-                        const dateTime = new Date(fs.statSync(filename).ctime);
-
-                        const frequency = parseInt(dirWatch.frequency, 10) || null;
-
-                        const system = parse(dirWatch.system);
-
-                        const talkgroup = parse(dirWatch.talkgroup);
-
-                        switch (dirWatch.type) {
-                            case 'trunk-recorder': {
-                                const metaFile = path.resolve(file.dir, `${file.name}.json`);
-
-                                const metaWatcher = chokidar.watch(metaFile, { awaitWriteFinish: true });
-
-                                const timeout = setTimeout(() => {
-                                    if (!metaWatcher.closed) {
-                                        metaWatcher.close();
-                                    }
-                                }, 60 * 60 * 1000);
-
-                                metaWatcher.on('add', async () => {
-                                    clearTimeout(timeout);
-
-                                    await metaWatcher.close();
-
-                                    try {
-                                        const meta = JSON.parse(fs.readFileSync(metaFile, 'utf8'));
-
-                                        await this.app.controller.importTrunkRecorder(audio, audioName, audioType, system, meta);
-
-                                    } catch (error) {
-                                        console.error(`Error loading file ${metaFile}`);
-
-                                        return;
-                                    }
-
-                                    if (dirWatch.deleteAfter) {
-                                        this.unlink(filename);
-                                        this.unlink(metaFile);
-                                    }
-                                });
-
-                                break;
-                            }
-
-                            case 'sdrtrunk': {
-                                const metaFile = path.resolve(file.dir, `${file.name}.mbe`);
-
-                                const metaWatcher = chokidar.watch(metaFile, { awaitWriteFinish: true });
-
-                                const timeout = setTimeout(() => {
-                                    if (!metaWatcher.closed) {
-                                        metaWatcher.close();
-                                    }
-                                }, 60 * 60 * 1000);
-
-                                metaWatcher.on('add', async () => {
-                                    clearTimeout(timeout);
-
-                                    await metaWatcher.close();
-
-                                    try {
-                                        const meta = JSON.parse(fs.readFileSync(metaFile, 'utf8'));
-
-                                        await this.app.controller.importSdrtrunk(audio, audioName, audioType, system, meta);
-
-                                    } catch (error) {
-                                        console.error(`Error loading file ${metaFile}`);
-
-                                        return;
-                                    }
-
-                                    if (dirWatch.deleteAfter) {
-                                        this.unlink(filename);
-                                        this.unlink(metaFile);
-                                    }
-                                });
-
-                                break;
-                            }
-
-                            default: {
-                                await this.app.controller.importCall({
-                                    audio,
-                                    audioName,
-                                    audioType,
-                                    dateTime,
-                                    frequency,
-                                    system,
-                                    talkgroup,
-                                });
-                            }
-
-                                if (dirWatch.deleteAfter) {
-                                    this.unlink(filename);
-                                }
-                        }
-                    }
-                });
-
-                this.watchers.push(watcher);
+                return;
             }
+
+            if (typeof dirWatch.extension !== 'string' || !dirWatch.extension.length) {
+                console.error(`Invalid dirWatch.extension: ${dirWatch.extension}`);
+
+                return;
+            }
+
+            if (typeof dirWatch === 'undefined' || !dirWatch.system === null) {
+                console.error(`Invalid dirWatch.system: ${dirWatch.system}`);
+
+                return;
+            }
+
+            const dir = path.resolve(__dirname, '../../..', dirWatch.directory);
+
+            const watcher = chokidar.watch(dir, {
+                awaitWriteFinish: true,
+                ignoreInitial: true,
+                recursive: true,
+                usePolling: typeof dirWatch.usePolling === 'boolean' ? dirWatch.usePolling : false,
+            });
+
+            watcher.on('add', (filename) => {
+                switch (dirWatch.type) {
+                    case 'trunk-recorder':
+                        this.importTrunkRecorderType(dirWatch, filename);
+                        break;
+
+                    case 'sdrtrunk':
+                        this.importSdrTrunkType(dirWatch, filename);
+                        break;
+
+                    default:
+                        this.importDefaultType(dirWatch, filename);
+                }
+            });
+
+            this.watchers.push(watcher);
         });
+    }
+
+    async importDefaultType(dirWatch, filename) {
+        const file = path.parse(filename);
+
+        if (file.ext === `.${dirWatch.extension}`) {
+            const audio = fs.readFileSync(filename);
+
+            const audioName = file.base;
+
+            const audioType = mime.lookup(file.base);
+
+            const dateTime = new Date(fs.statSync(filename).ctime);
+
+            const frequency = parseInt(dirWatch.frequency, 10) || null;
+
+            const system = this.parseId(dirWatch.system, filename);
+
+            const talkgroup = this.parseId(dirWatch.talkgroup, filename);
+
+            await this.app.controller.importCall({
+                audio,
+                audioName,
+                audioType,
+                dateTime,
+                frequency,
+                system,
+                talkgroup,
+            });
+
+            if (dirWatch.deleteAfter) {
+                this.unlink(filename);
+            }
+        }
+    }
+
+    async importSdrTrunkType(dirWatch, filename) {
+        const file = path.parse(filename);
+
+        if (file.ext === '.mbe') {
+            const audioFile = path.resolve(file.dir, `${file.name}.${dirWatch.extension}`);
+
+            if (fs.existsSync(audioFile)) {
+                const audio = fs.readFileSync(audioFile);
+
+                const audioName = file.base;
+
+                const audioType = mime.lookup(audioFile);
+
+                const system = this.parseId(dirWatch.system, filename);
+
+                try {
+                    const meta = JSON.parse(fs.readFileSync(filename, 'utf8'));
+
+                    await this.app.controller.importSdrtrunk(audio, audioName, audioType, system, meta);
+
+                    if (dirWatch.deleteAfter) {
+                        this.unlink(audioFile);
+
+                        this.unlink(filename);
+                    }
+
+                } catch (error) {
+                    console.error(`Error loading file ${filename}`);
+
+                    return;
+                }
+            }
+        }
+    }
+
+    async importTrunkRecorderType(dirWatch, filename) {
+        const file = path.parse(filename);
+
+        if (file.ext === '.json') {
+            const audioFile = path.resolve(file.dir, `${file.name}.${dirWatch.extension}`);
+
+            if (fs.existsSync(audioFile)) {
+                const audio = fs.readFileSync(audioFile);
+
+                const audioName = file.base;
+
+                const audioType = mime.lookup(audioFile);
+
+                const system = this.parseId(dirWatch.system, filename);
+
+                try {
+                    const meta = JSON.parse(fs.readFileSync(filename, 'utf8'));
+
+                    await this.app.controller.importTrunkRecorder(audio, audioName, audioType, system, meta);
+
+                    if (dirWatch.deleteAfter) {
+                        this.unlink(audioFile);
+
+                        this.unlink(filename);
+                    }
+
+                } catch (error) {
+                    console.error(`Error loading file ${filename}`);
+
+                    return;
+                }
+            }
+        }
+    }
+
+    parseId(id, filename) {
+        if (typeof id === 'number') {
+            return id;
+
+        } else if (typeof id === 'string') {
+            try {
+                const regExp = new RegExp(id);
+
+                return parseInt(filename.replace(regExp, '$1'), 10);
+
+            } catch (error) {
+                console.log(`Invalid RegExp: ${id}`);
+
+                return NaN;
+            }
+
+        } else {
+            return NaN;
+        }
     }
 
     unlink(filename) {
