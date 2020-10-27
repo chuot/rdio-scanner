6be3c3a0deca95c6a7022cd6961233d3f30eeca5
Scott Weis KB2EAR <kb2ear@kb2ear.net>
2020-10-27 19:47:16 -0400

    Update upload-call.sh
    Quiet down logging and hold files for 10 seconds to allow upload to other services.

--- docs/examples/trunk-recorder/upload-call.sh
+++ docs/examples/trunk-recorder/upload-call.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+PATH=$PATH:/usr/local/bin:/usr/bin:/bin
 basename="${2%.*}"
 
 curl http://127.0.0.1:3000/api/trunk-recorder-call-upload \
      -F "key=b29eb8b9-9bcd-4e6e-bb4f-d244ada12736" \
      -F "audio=@${basename}.wav;type=audio/wav" \
      -F "meta=@${basename}.json;type=application/json" \
-     -F "system=${1:-0}"
+     -F "system=${1:-0}" --silent --show-error
 
+sleep 10 #allows for upload to openmhz and radioreference calls
 rm -f ${basename}.*
